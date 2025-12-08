e6afa6918d000c50b90b490dbe01f72ed6687361
milinkanu <milinrkanu999@gmail.com>
2025-12-08 13:31:27 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,6 +2,8 @@ const asyncHandler = (fn) => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
 };
 
+export { asyncHandler };
+
 // const asyncHandler = (fn) => async(req, res, next) => {
 //     try {
 //         await fn(req, res, next)
