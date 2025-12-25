fde0c608d117996681fcf81b3be210cb57505a11
milinkanu <milinrkanu999@gmail.com>
2025-12-25 20:33:33 +0530

    feat: Implement video, comment, like, tweet, playlist, subscription, and dashboard controllers and routes
    - Added comment controller with CRUD operations for video comments.
    - Implemented dashboard controller to fetch channel statistics and videos.
    - Created healthcheck controller for service status.
    - Developed like controller to toggle likes on videos, comments, and tweets.
    - Introduced playlist controller for managing user playlists.
    - Added subscription controller to handle user subscriptions to channels.
    - Created tweet controller for managing user tweets.
    - Implemented video controller for video management including publishing, updating, and deleting.
    - Established routes for comments, dashboard, healthcheck, likes, playlists, subscriptions, tweets, and videos.

---
+++ src/routes/subscription.routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels,
+} from "../controllers/subscription.controller.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+
+const router = Router();
+
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router.route("/c/:channelId").post(toggleSubscription).get(getUserChannelSubscribers);
+router.route("/u/:subscriberId").get(getSubscribedChannels);
+
+export default router
